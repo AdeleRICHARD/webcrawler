175a55ebd2c712bf84a165ea538dcecb83a198af
AdeleRICHARD <adrichard@figarocms.fr>
2025-01-09 16:17:53 +0100

    first occ of tests + functions

---
+++ normalize_url.go
@@ -0,0 +1,74 @@
+package main
+
+import (
+	"errors"
+	"fmt"
+	"net/url"
+	"path"
+	"strings"
+
+	"golang.org/x/net/html"
+)
+
+func normalizeURL(inputUrl string) (string, error) {
+	if inputUrl == "" {
+		return "", errors.New("InpurUrl is empty")
+	}
+	parsedURL, err := url.Parse(inputUrl)
+	if err != nil {
+		return "", err
+	}
+
+	cleanedPath := path.Clean(parsedURL.Path)
+	if !strings.HasPrefix(cleanedPath, "/") {
+		cleanedPath = "/" + cleanedPath
+	}
+
+	normalizedURL := parsedURL.Host + cleanedPath
+	normalizedURL = strings.ReplaceAll(normalizedURL, " ", "")
+
+	return normalizedURL, nil
+}
+
+func getURLsFromHTML(htmlBody, rawBaseURL string) ([]string, error) {
+	//tokenizer := html.NewTokenizer(strings.NewReader(htmlBody))
+	var urls []string
+
+	/* for {
+		tokenType := tokenizer.Next()
+		if tokenType == html.ErrorToken {
+			return nil, tokenizer.Err()
+		}
+
+		switch tokenType {
+		case html.StartTagToken, html.SelfClosingTagToken:
+			token := tokenizer.Token()
+			if token.Data == "a" {
+				urls = append(urls, token.String())
+			}
+			fmt.Println(token.Data)
+		case html.ErrorToken:
+			return urls, nil
+		}
+	} */
+
+	page, err := html.Parse(strings.NewReader(htmlBody))
+	if err != nil {
+		return nil, err
+	}
+
+	for node := range page.Descendants() {
+		if node.Type == html.ElementNode && node.Data == "a" {
+			fmt.Println(node.Data)
+			for _, attr := range node.Attr {
+				if attr.Key == "href" {
+					urls = append(urls, attr.Val)
+					break
+				}
+			}
+		}
+	}
+
+	return urls, nil
+
+}
---
+++ normalize_url_test.go
@@ -0,0 +1,159 @@
+package main
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestNormalizeURL(t *testing.T) {
+	t.Run("remove scheme", func(t *testing.T) {
+		expected := "blog.boot.dev/path"
+		inputUrl := "https://blog.boot.dev/path"
+
+		got, err := normalizeURL(inputUrl)
+		assert.NoError(t, err)
+		assert.Equal(t, expected, got)
+	})
+
+	t.Run("remove / at the end + scheme", func(t *testing.T) {
+		expected := "blog.boot.dev/path"
+		inputUrl := "https://blog.boot.dev/path/"
+
+		got, err := normalizeURL(inputUrl)
+		assert.NoError(t, err)
+		assert.Equal(t, expected, got)
+	})
+
+	t.Run("remove scheme http", func(t *testing.T) {
+		expected := "blog.boot.dev/path"
+		inputUrl := "http://blog.boot.dev/path"
+
+		got, err := normalizeURL(inputUrl)
+		assert.NoError(t, err)
+		assert.Equal(t, expected, got)
+	})
+
+	t.Run("remove scheme http + / at the end", func(t *testing.T) {
+		expected := "blog.boot.dev/path"
+		inputUrl := "http://blog.boot.dev/path/"
+
+		got, err := normalizeURL(inputUrl)
+		assert.NoError(t, err)
+		assert.Equal(t, expected, got)
+	})
+
+	t.Run("remove double /", func(t *testing.T) {
+		expected := "blog.boot.dev/path"
+		inputUrl := "https://blog.boot.dev//path"
+
+		got, err := normalizeURL(inputUrl)
+		assert.NoError(t, err)
+		assert.Equal(t, expected, got)
+	})
+
+	t.Run("remove double dash at the end", func(t *testing.T) {
+		expected := "blog.boot.dev/path"
+		inputUrl := "https://blog.boot.dev/path//"
+
+		got, err := normalizeURL(inputUrl)
+		assert.NoError(t, err)
+		assert.Equal(t, expected, got)
+	})
+
+	t.Run("remove spaces", func(t *testing.T) {
+		expected := "blog.boot.dev/path"
+		inputUrl := "https://blog.boot.dev/ path"
+
+		got, err := normalizeURL(inputUrl)
+		assert.NoError(t, err)
+		assert.Equal(t, expected, got)
+	})
+
+	t.Run("return error if input empty", func(t *testing.T) {
+		inputUrl := ""
+
+		_, err := normalizeURL(inputUrl)
+		assert.Error(t, err)
+	})
+
+}
+
+func TestGetURLsFromHTML(t *testing.T) {
+	t.Run("Exact two links from page", func(t *testing.T) {
+		inputURL := "https://blog.boot.dev"
+		inputHtml := `
+<html>
+	<body>
+		<a href="/path/one">
+			<span>Boot.dev</span>
+		</a>
+		<a href="https://other.com/path/one">
+			<span>Boot.dev</span>
+		</a>
+	</body>
+</html>
+`
+		expected := []string{"https://blog.boot.dev/path/one", "https://other.com/path/one"}
+		got, err := getURLsFromHTML(inputHtml, inputURL)
+		assert.NoError(t, err)
+		assert.Equal(t, expected, got)
+	})
+
+	t.Run("Exact one link from page", func(t *testing.T) {
+		inputURL := "https://blog.boot.dev"
+		inputHtml := `
+<html>
+	<body>
+		<a href="/path/one">
+			<span>Boot.dev</span>
+		</a>
+	</body>
+</html>
+`
+		expected := []string{"https://blog.boot.dev/path/one"}
+		got, err := getURLsFromHTML(inputHtml, inputURL)
+		assert.NoError(t, err)
+		assert.Equal(t, expected, got)
+	})
+
+	t.Run("Exact 0 link from page", func(t *testing.T) {
+		inputURL := "https://blog.boot.dev"
+		inputHtml := `
+<html>
+	<body>
+		<p> no href </p> 
+	</body>
+</html>
+`
+		got, err := getURLsFromHTML(inputHtml, inputURL)
+		assert.NoError(t, err)
+		assert.Nil(t, got)
+	})
+
+	t.Run("Return an error if inputURL empty", func(t *testing.T) {
+		inputURL := ""
+		inputHtml := `
+<html>
+	<body>
+		<p> no href </p> 
+	</body>
+</html>
+`
+		_, err := getURLsFromHTML(inputHtml, inputURL)
+		assert.Error(t, err)
+	})
+
+	t.Run("Return error if body empty", func(t *testing.T) {
+		inputURL := "https://blog.boot.dev"
+		inputHtml := `
+<html>
+	<body>
+	</body>
+</html>
+`
+		got, err := getURLsFromHTML(inputHtml, inputURL)
+		assert.Error(t, err)
+		assert.Nil(t, got)
+	})
+}
