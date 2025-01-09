e26631765cf1d41667d7780d96b51858a41ecccf
AdeleRICHARD <adrichard@figarocms.fr>
2025-01-09 16:40:56 +0100

    Implement cli

--- main.go
+++ main.go
@@ -1,7 +1,24 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+)
 
 func main() {
-	fmt.Println("Hello, World!")
+	args := os.Args[1:]
+	fmt.Println(args)
+	if len(args) < 1 {
+		fmt.Println("no website provided")
+		os.Exit(1)
+	}
+
+	if len(args) > 1 {
+		fmt.Println("too many arguments provided")
+		os.Exit(1)
+	}
+
+	if len(args) == 1 {
+		fmt.Println("starting crawl of: ", args[0])
+	}
 }
